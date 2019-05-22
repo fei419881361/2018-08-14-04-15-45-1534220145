a515b2233e11857a5cd5ee3f18f82912a1ef9421
cs_zlf <419881361@qq.com>
2019-05-22 10:29:37 +0800

    完成

--- main/main.js
+++ main/main.js
@@ -1,4 +1,7 @@
 // Write your cade below:
-module.exports = function main() {
-  
+module.exports = function main(numA, numB) {
+    if(numB == 0){
+        return -1; //除数不能为0
+    }
+  return numA%numB;
 };
